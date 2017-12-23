75057c478495bf61e48955150701e30845ef21b0
Yves Matta <yves.matta@qochealth.com>
2017-12-22 22:01:09 -0500

    finished auth flow

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+if (proccess.env.NODE_ENV === 'production') {
+  modules.exports = require('./prod');
+} else {
+  modules.exports = require('./dev');
+}
--- index.js
+++ index.js
@@ -1,32 +1,24 @@
+const cookieSession = require('cookie-session');
 const express = require('express');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
 
+mongoose.connect(keys.mongoURI);
 const app = express();
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log('accessToken', accessToken);
-      console.log('refreshToken', refreshToken);
-      console.log('profile', profile);
-    }
-  )
-);
-
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
   })
 );
 
-app.get('/auth/google/callback', passport.authenticate('google'));
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
