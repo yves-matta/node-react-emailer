db4ace1aaa1a97f6eaba3230eabf24d354e4c0b0
Yves Matta <yves.matta@qochealth.com>
2017-12-22 22:05:08 -0500

    fix spelling

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-if (proccess.env.NODE_ENV === 'production') {
-  modules.exports = require('./prod');
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./prod');
 } else {
-  modules.exports = require('./dev');
+  module.exports = require('./dev');
 }
