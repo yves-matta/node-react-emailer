9bb5b42e963cdb145c8974ce1cc818a7167ea7c8
Yves Matta <Yves.Matta7@gmail.com>
2018-01-07 01:51:20 -0500

    fix project

--- server/config/prod.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  cookieKey: process.env.COOKIE_KEY,
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI
-};
--- services/passport.js
+++ services/passport.js
@@ -19,16 +19,15 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleID: profile.id }).then(existingUser => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          new User({ googleID: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleID: profile.id });
+
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+
+      const user = await new User({ googleID: profile.id }).save();
+      return done(null, user);
     }
   )
 );
